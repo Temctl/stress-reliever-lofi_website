94b2dbade427883d08eaa6fa277dd3113a132a9e
Temuulen Ulziisaikhan <utemuka@gmail.com>
2021-11-12 22:05:33 -0700

    few bugs here and i am gonna start doing const as a object.

---
+++ Snowflake.js
@@ -0,0 +1,20 @@
+class Snowflake{
+
+    /*constructor(){
+        var xAxis = float.random(0, width); 
+        var yAxis = float.random(-25, -5);
+        var mass = float.random(2, 15); // this is gonna determine how big the snowflake is
+        //and which will also determine how fast it will fall
+        var speed = 15 + mass;
+        var direction = random([-0.3, 0.3]); // snowflakes will either fall right way or left way slanted
+    
+    }*/
+
+    fall(){
+        //ellipse(xAxis + direction, yAxis - speed, mass, mass);
+        ellipse(50, 50, 50, 50);
+    }
+  
+  
+  
+}
--- sketch.js
+++ sketch.js
@@ -1,22 +1,57 @@
 let input, button, greeting, gif;
+//do the snow gif
+//add music (soundcloud api?, )
 
-function preload(){
-  gif = loadImage('rainy(1).gif');
-}
+var width;
+var height;
 
 function setup() {
-  var width = windowWidth;
-  var height = windowHeight;
+  sn = new Snowflake;
 
+  width = windowWidth;
+  height = windowHeight;
+  
   createCanvas(width, height);
 
   input = createInput();
-  input.position(width/2, height/2);
+  input.size(100, 20);
+  input.position(width/2 - 50, height/2);
 
   button = createButton('talk');
   button.position(input.x + input.width, height/2)
+  ellipse(50, 50, 50, 50);
+
+  background();
 }
 
+// gonna do responds
+
+
+function draw() {
+  //sn.fall();
+  
+}
+
+const Snowflake = {
+
+  xAxis = float.random(0, width),
+  yAxis = float.random(-25, -5),
+  mass = float.random(2, 15), // this is gonna determine how big the snowflake is
+  //and which will also determine how fast it will fall
+  speed = 15 + mas,
+  direction = random([-0.3, 0.3]), // snowflakes will either fall right way or left way slanted
+
+  fall : function(){
+    //ellipse(xAxis + direction, yAxis - speed, mass, mass);
+    ellipse(50, 50, 50, 50);
+  }
+
+
+
+}
+
+
+//talking stuff
 function talking(){
   button.mouseClicked.Pressed(talking);
 
@@ -28,8 +63,4 @@ function respond(){
 
 }
 
-function draw() {
-  background(220);
-  image(gif, 0, 0);
-  gif.play();
-}
+
